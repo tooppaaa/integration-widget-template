a5f8e57d79727db336d3611ac733fab243df2410
Mickael Kehyayan <mkehyayan@talentsoft.com>
2019-04-11 18:05:55 +0200

    TSM-234: add List.test.tsx (5 tests)

---
+++ app/templates/List/List.test.tsx
@@ -0,0 +1,66 @@
+import { mount, shallow } from 'enzyme';
+import * as React from 'react';
+import { List, Line } from './List';
+
+describe('Widget List', () => {
+    let mockedProps: Line[];
+
+    beforeAll(() => {
+        mockedProps = [];
+        mockedProps.push({id: 1, 
+            urlPicture: '', 
+            title: 'title', 
+            subtitle: 'subtitle', 
+            description: 'description',
+            detail: true});
+    });
+
+    afterEach(() => {
+        const wrapper = shallow(<List showPicture={false} showDetail={false} values={mockedProps} />);
+
+        expect(wrapper.exists('.line-title')).toBe(true);
+        expect(wrapper.exists('.subtitle')).toBe(true);
+        expect(wrapper.exists('.description')).toBe(true);
+    });
+
+    it('With showPicture = true, Check if urlPicture is null/empty, show default icon', () => {
+        const wrapper = shallow(<List showPicture={true} showDetail={false} values={mockedProps} />);
+        
+        expect(wrapper.exists('.icon-profil')).toBe(true);
+        expect(wrapper.exists('.avatar-picture')).toBe(false);
+    });
+
+    it('With showPicture = true, Check if urlPicture is not null/empty, show image', () => {
+        mockedProps[0].urlPicture = 'UrlForImage';
+        const wrapper = shallow(<List showPicture={true} showDetail={false} values={mockedProps} />);
+        
+        expect(wrapper.exists('.icon-profil')).toBe(false);
+        expect(wrapper.exists('.avatar-picture')).toBe(true);
+    });
+
+    it('With showPicture = false, Check if urlPicture or icon is not display', () => {
+        mockedProps[0].urlPicture = 'UrlForImage';
+        const wrapper = shallow(<List showPicture={false} showDetail={false} values={mockedProps} />);
+        
+        expect(wrapper.exists('.icon-profil')).toBe(false);
+        expect(wrapper.exists('.avatar-picture')).toBe(false);
+    });
+
+    it('With showDetail = true, Check if detail is displayed with the right color', () => {
+        mockedProps[0].detail = true
+        const wrapper = shallow(<List showPicture={true} showDetail={true} values={mockedProps} />);
+        
+        expect(wrapper.find('.detail').length).toBe(1);
+        expect(wrapper.find('.detail').hasClass('green')).toBe(true);
+        expect(wrapper.find('.detail').hasClass('red')).toBe(false);
+    });
+
+    it('With showDetail = true, Check if detail is displayed with the right color', () => {
+        mockedProps[0].detail = false
+        const wrapper = shallow(<List showPicture={true} showDetail={true} values={mockedProps} />);
+        
+        expect(wrapper.find('.detail').length).toBe(1);
+        expect(wrapper.find('.detail').hasClass('green')).toBe(false);
+        expect(wrapper.find('.detail').hasClass('red')).toBe(true);
+    });
+});
